531f99e93e738aebd537bd8388f8a0e079870995
MuriithiMark <markmwangi14317@gmail.com>
2024-02-22 11:43:47 +0300

    routing

--- express/2.routing/src/index.js
+++ express/2.routing/src/index.js
@@ -3,8 +3,31 @@ import express from "express";
 const app = express()
 const PORT = process.env.PORT ?? 3000;
 
-app.get("/", (req, res) => res.send('Hello'))
-// listen(port, hostname, () => (runs once when server starts))
-app.listen(PORT,() => {
+// HTTP Methods
+// GET    - retrive data from server, serve data from server
+// POST   - send data to the server from client
+// PUT    - send data to server to perform updates of entire resource based on its unique identifier -> mainly the id
+//        - all fields of resource to be updated must be present, if a particular field is missing it will be null
+// PATCH  - send partial data updates for resource based on unique identifier of the resource
+// DELETE - deletes the record completely
+//
+
+const homeHTML = (obj) => {
+    return (
+        `
+        <h1>${obj.title}</h1>
+        <p>Hello ${obj.person.name}</p>
+        `
+    )
+}
+app.get("/", (req, res) => {
+    res.send(homeHTML({
+        title: 'Portofolio', person: {
+            name: 'Mark Mwangi'
+        }
+    }))
+})
+
+app.listen(PORT, () => {
     console.info(`Server running at http://localhost:${PORT}`)
 })
